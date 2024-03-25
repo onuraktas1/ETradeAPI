5dfcbc3633dafaff9a64d53961e7be4d3000bf40
Onur Aktas <onuraktas0640@gmail.com>
2024-03-26 00:01:35 +0300

    Test

---
+++ Infrastructure/ETradeAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using ETradeAPI.Application.Abstractions;
+using ETradeAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        => new()
+        {
+            new(){ Id=Guid.NewGuid(),Name="Product 1",Price=250, Stock=20, CreatedDate=DateTime.Now},
+            new(){ Id=Guid.NewGuid(),Name="Product 2",Price=450, Stock=40, CreatedDate=DateTime.Now},
+            new(){ Id=Guid.NewGuid(),Name="Product 3",Price=550, Stock=60, CreatedDate=DateTime.Now},
+            new(){ Id=Guid.NewGuid(),Name="Product 4",Price=650, Stock=220, CreatedDate=DateTime.Now},
+            new(){ Id=Guid.NewGuid(),Name="Product 5",Price=750, Stock=50, CreatedDate=DateTime.Now},
+        };
+    }
+}
--- Presentation/ETradeAPI.API/Program.cs
+++ Presentation/ETradeAPI.API/Program.cs
@@ -1,3 +1,5 @@
+using ETradeAPI.Persistence;
+
 namespace ETradeAPI.API
 {
     public class Program
@@ -6,16 +8,14 @@ namespace ETradeAPI.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.AddPersistenceServices();
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
