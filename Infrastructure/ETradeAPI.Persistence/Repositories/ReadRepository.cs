70f794056a365ab354fea5f3072af7848b70b012
Onur Aktas <onuraktas0640@gmail.com>
2024-03-27 22:11:40 +0300

    Crud

---
+++ Infrastructure/ETradeAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,38 @@
+﻿using ETradeAPI.Application.Repositories;
+using ETradeAPI.Domain.Entities.Common;
+using ETradeAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETradeAPI.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+
+        private readonly ETradeAPIDbContext _context;
+        public ReadRepository(ETradeAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll()
+        => Table;
+
+        public async Task<T> GetAsync(string id)
+       => await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method)
+        => await Table.FirstOrDefaultAsync(method);
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method)
+        => Table.Where(method);
+    }
+}
